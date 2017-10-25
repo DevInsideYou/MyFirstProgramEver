e75a94c3b4a1809761825c34be435ab52331ecbc
Vladyslav Pekker <agilesteel@gmail.com>
2017-10-25 13:16:23 +0200

    Added Clojure.

---
+++ MyFirstProgramEverJVM/src/main/clojure/MyFirstClojureProgramEver.clj
@@ -0,0 +1,24 @@
+(defn asleep
+  ([hour] (message hour "asleep")))
+
+(defn awake
+  ([hour] (message hour "awake")))
+
+(defn message
+  ([hour state] (str "It's " hour " o'clock, so I'm " state)))
+
+(defn while
+  ([isWorkday currentHour limit]
+    (if(== currentHour limit) nil
+    (cons (if isWorkday
+            (cond
+              (< currentHour 7) (asleep currentHour)
+              (< currentHour 23) (awake currentHour)
+              :else (asleep currentHour))
+            (if
+              (< currentHour 9) (asleep currentHour)
+                                (awake currentHour)))
+          (while isWorkday (+ currentHour 1) limit)))))
+
+(doseq [item (while true 0 #_("12am") 24)]
+ (println item))
